c9982f14623c29d54264aed378329285741b1e25
ys.kim <kys8411@gmail.com>
2026-03-15 14:48:26 +0900

    docs: 1주차 학습 로그 추가

---
+++ my_study_log.py
@@ -0,0 +1,9 @@
+study_log = {
+    "날짜": "2026-03-15",
+    "주차": "1주차",
+    "오늘 배운 것": "git init, add, commit, status, log, diff",
+    "느낀 점": "Git이 생각보다 어렵지 않다!",
+}
+
+for key, value in study_log.items():
+    print(f"{key}: {value}")
