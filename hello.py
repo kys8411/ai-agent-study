d8beec30b89f7d1933ba2d897c7df52b47bf5caf
ys.kim <kys8411@gmail.com>
2026-03-15 21:40:37 +0900

    feat: greet 함수 추가

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("아 이거였구나...!")
+print("Hello AI Agent!")
+print("오늘부터 Git을 배웁니다!")
+print("잼있다!!!!.")
+
+def greet(name):
+    print(f"안녕하세요, {name}님! AI 에이전트 스터디에 오신 걸 환영합니다!")
+
+greet("YongSeok")
