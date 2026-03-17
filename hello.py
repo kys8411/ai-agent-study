13c7cb4bc2852d39438a74bf6795d93b9d9fc307
ys.kim <kys8411@gmail.com>
2026-03-18 06:09:10 +0900

    main에서 수정

--- hello.py
+++ hello.py
@@ -1,11 +1,1 @@
-print("hello from FEATURE")
-
-
-print("Hello AI Agent!")
-print("오늘부터 Git을 배웁니다!")
-print("잼있다!!!!.")
-
-def greet(name):
-    print(f"안녕하세요, {name}님! AI 에이전트 스터디에 오신 걸 환영합니다!")
-
-greet("YongSeok")
+print("Hello from MAIN")
